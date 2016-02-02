d076b5a62fb21ce2d1b9c9a20e18092f50f13d63
David Ke Hong <hongkedavid@gmail.com>
2016-02-02 14:49:27 -0500

    Update svm_train.sh

--- scripts/svm_train.sh
+++ scripts/svm_train.sh
@@ -1,2 +1,8 @@
+# generate SVM training input
+./gen_svm_input.sh 4000 codec-vp9-nexus6.qoe svm-vp9-nexus6-correlate.train
+
+# scale training data to [0, 1]
 ./svm-scale -l 0 -u 1 -s range svm-vp9-nexus6-train > svm-vp9-nexus6-train-scale
+
+# compute SVM classifier
 ./svm-train -s 0 svm-vp9-nexus6-train-scale
