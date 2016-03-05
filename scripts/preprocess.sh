28a37059072ec6f90b50ae103fa9bb5be42a45ee
David Ke Hong <hongkedavid@gmail.com>
2016-03-05 01:41:25 -0500

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,5 +1,14 @@
+# profile thread metrics
 sh filter_proc_tid.sh sorted.all.nexus6.activem.decoded 2278 > codec-vp9-nexus6-active.tid
 sh profile_cpu_lock.sh
 
+# compute thread-qoe correlation
+sh compute_thread_corr.sh 4000 codec-vp9-nexus6.qoe
+
 # generate SVM/DT training input
 ./gen_svm_input.sh 4000 codec-vp9-nexus6.qoe svm-vp9-nexus6-correlate.train
+
+# get all thread IDs
+sh get_thread_id.sh sorted.all.nexus6.activem.decoded
+
+sh plot_thread.sh 4000 codec-vp9-nexus6.qoe
