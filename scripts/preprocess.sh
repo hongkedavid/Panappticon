16a510368fed61ff9c7c29c6bb8c8016d7008b27
David Ke Hong <hongkedavid@gmail.com>
2016-03-05 18:10:29 -0500

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -16,3 +16,6 @@ sh plot_thread.sh 4000 codec-vp9-nexus6.qoe
 
 sh get_futex_event sorted.all.nexus6.activem.decoded $tid
 cat futext_event.$tid1 | python ComputeFutexTime.py $tid2 > $tid1_$tid2.out
+
+sh profile_binder.sh codec-vp9-nexus6-active.tid
+sh compute_binder_corr.sh 4000 codec-vp9-nexus6.qoe
