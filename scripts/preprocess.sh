c5308516cde674ba67772c49f979a1bc1e949220
David Ke Hong <hongkedavid@gmail.com>
2016-03-06 20:44:45 -0500

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -5,6 +5,10 @@ sh profile_cpu_lock.sh
 # compute thread-qoe correlation
 sh compute_thread_corr.sh 4000 codec-vp9-nexus6.qoe
 
+# get all thread IDs
+sh get_thread_id.sh sorted.all.nexus6.activem.decoded > all.tid
+./unify_feature.sh 0 $start $end
+
 # generate SVM training input
 ./gen_svm_input.sh 4000 codec-vp9-nexus6.qoe svm-vp9-nexus6-correlate.train
 
@@ -12,14 +16,13 @@ sh compute_thread_corr.sh 4000 codec-vp9-nexus6.qoe
 ./gen_svm_input.sh 4000 codec-vp9-nexus6.qoe svm-vp9-nexus6-train.csv
 python dtree.py svm-vp9-nexus6-train.csv
 
-# get all thread IDs
-sh get_thread_id.sh sorted.all.nexus6.activem.decoded > all.tid
-./unify_feature.sh 0 $start $end
-
+# plot thread metrics vs. QoE
 sh plot_thread.sh 4000 codec-vp9-nexus6.qoe
 
+# profile futex blocking dependency
 sh get_futex_event sorted.all.nexus6.activem.decoded $tid
 cat futext_event.$tid1 | python ComputeFutexTime.py $tid2 > $tid1_$tid2.out
 
+# profile binder
 sh profile_binder.sh codec-vp9-nexus6-active.tid
 sh compute_binder_corr.sh 4000 codec-vp9-nexus6.qoe
