d79db6b500e1878bfecfa3e78ce40e0a1e07eca4
David Ke Hong <hongkedavid@gmail.com>
2016-03-20 00:38:56 -0400

    Update install_android.sh

--- script/install_android.sh
+++ script/install_android.sh
@@ -16,14 +16,18 @@ wget https://dl.google.com/dl/android/aosp/broadcom-hammerhead-lmy48m-5d6ca8e6.t
 wget https://dl.google.com/dl/android/aosp/lge-hammerhead-lmy48m-0759ba99.tgz
 wget https://dl.google.com/dl/android/aosp/qcom-hammerhead-lmy48m-b7143e92.tgz
 tar xf broadcom-hammerhead-lmy48m-5d6ca8e6.tgz
-./
+tar xf lge-hammerhead-lmy48m-0759ba99.tgz
+tar xf qcom-hammerhead-lmy48m-b7143e92.tgz
+./extract-broadcom-hammerhead.sh  
+./extract-lge-hammerhead.sh  
+./extract-qcom-hammerhead.sh
 
 # replace the kernel (location can be found at https://source.android.com/source/building-kernels.html) and tc source file
 cd msm
 cp arch/arm/boot/zImage-dtb android-5.1.1_r14/device/lge/hammerhead-kernel/
-
+cp q_netem.c tc.c Android.mk android-5.1.1_r14/external/iproute2/tc/
 
 # build Android source
 source build/envsetup.sh 
-lunch (full_hammerhead-userdebug)
+lunch (aosp_hammerhead-userdebug)
 make -j32
