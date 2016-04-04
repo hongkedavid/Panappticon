4ff4e02366749d2e968324b4e643ac2c8c6c5d30
David Ke Hong <hongkedavid@gmail.com>
2016-04-04 17:14:21 -0400

    Update rate_limit.sh

--- scripts/rate_limit.sh
+++ scripts/rate_limit.sh
@@ -8,6 +8,10 @@ tc class add dev wlan0 parent 1: classid 1:2 htb rate 10mbit
 tc filter add dev wlan0 protocol ip parent 1:0 prio 1 u32 match ip dst 141.212.110.134/32 flowid 1:1
 tc filter add dev wlan0 protocol ip parent 1:0 prio 1 u32 match ip src 141.212.110.134/32 flowid 1:2
 
+tc qdisc add dev wlan0 root handle 1: tbf default 30
+tc qdisc add dev wlan0 parent 1: classid 1:1 tbf rate 10mbit burst 20kb limit 20ms
+tc filter add dev wlan0 protocol ip parent 1:0 prio 1 u32 match ip dst 141.212.110.134/32 flowid 1:1
+
 # show rules
 tc -s qdisc
 
