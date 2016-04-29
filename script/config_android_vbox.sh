06e5881d3d0af8503e3effd829c74475445aaf6e
David Ke Hong <hongkedavid@gmail.com>
2016-04-28 23:34:44 -0400

    Create config_android_vbox.sh

---
+++ script/config_android_vbox.sh
@@ -0,0 +1,10 @@
+# Follow http://linus.nci.nih.gov/bdge/installUbuntu.html to install Ubuntu 14.04 on VBox
+
+sudo apt-get install vim git-core gnupg flex bison gperf build-essential \
+  zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
+  lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
+  libgl1-mesa-dev libxml2-utils xsltproc unzip abootimg openjdk-7-jdk
+  
+sudo su
+wget -S -O - http://source.android.com/source/51-android.rules | sed "s/<username>/$USER/" | sudo tee >/dev/null /etc/udev/rules.d/51-android.rules; sudo udevadm control --reload-rules
+
