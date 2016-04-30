80274719e275bf5ff96611f08008237607966922
David Ke Hong <hongkedavid@gmail.com>
2016-04-30 00:48:44 -0400

    Create factory_flash.sh

---
+++ script/factory_flash.sh
@@ -0,0 +1,9 @@
+# Android-5.1.1 factory image
+
+./flash-base.sh
+fastboot flash recovery recovery.img
+fastboot flash boot boot.img
+fastboot flash system system.img
+fastboot flash cache cache.img
+fastboot flash userdata userdata.img
+fastboot reboot
