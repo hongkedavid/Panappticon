81be631a6cb7de7510141309ce2fdcb7a4f2ac05
David Ke Hong <hongkedavid@gmail.com>
2016-05-04 12:58:21 -0700

    Update install_pcalg.R

--- script/install_pcalg.R
+++ script/install_pcalg.R
@@ -1,3 +1,11 @@
+# Update R package to the laest one at http://sysads.co.uk/2014/06/install-r-base-3-1-0-ubuntu-14-04/
+
 library("utils")
+# will encounter problems in missing pkgs 
 install.packages("pcalg")
-# install.packages("pcalg_2.2-4.tar.gz", repos = NULL, type = "source", dependencies = TRUE) # doesn't work due to missing dependency
+# install graph and RBGL
+source("http://bioconductor.org/biocLite.R") 
+biocLite("RBGL")
+#browseVignettes("graph")
+install.packages("igraph", dependencies=TRUE)
+# install.packages("/home/david/plyr_1.8.3.tar.gz", repos = NULL, type = "source", dependencies = TRUE) # doesn't work due to missing dependency
