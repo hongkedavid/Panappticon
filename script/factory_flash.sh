d3dd2a7b9f557fa87d3fd7892a1455b806935309
David Ke Hong <hongkedavid@gmail.com>
2016-05-07 15:42:26 -0700

    Update factory_flash.sh

--- script/factory_flash.sh
+++ script/factory_flash.sh
@@ -1,10 +1,16 @@
 # Android-5.1.1 factory image
 
-./flash-base.sh
+#./flash-base.sh
+fastboot flash bootloader bootloader-shamu-moto-apq8084-71.10.img
+fastboot reboot-bootloader
+fastboot flash radio radio-shamu-d4.0-9625-02.101.img
+fastboot reboot-bootloader
+
+fastboot flash cache cache.img
+
 fastboot flash recovery recovery.img
 fastboot flash boot boot.img
 fastboot flash system system.img
-fastboot flash cache cache.img
 
 # Erase user data if executed
 # fastboot flash userdata userdata.img
