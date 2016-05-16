6a33d68ac0922b591248f365ee46b2f6c5f3f3b9
David Ke Hong <hongkedavid@gmail.com>
2016-05-16 11:37:05 -0700

    Update atrace.sh

--- script/atrace.sh
+++ script/atrace.sh
@@ -5,5 +5,5 @@ echo 1 > /sys/kernel/debug/tracing/events/binder/enable
 # enable tracing all in atrace
 echo 1 > /sys/kernel/debug/tracing/events/enable 
 
-# start tracing for 10 sec
-atrace -t 10 > /sdcard/atrace.dump
+# start tracing for 10 sec with buffer size 100MB (2MB by default)
+atrace -t 10 -b 102400 > /sdcard/atrace.dump
