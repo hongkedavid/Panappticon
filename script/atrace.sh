67b9f01ad808f2ba9ff03041d5e4f61a3eac6d45
David Ke Hong <hongkedavid@gmail.com>
2016-05-16 23:34:36 -0700

    Update atrace.sh

--- script/atrace.sh
+++ script/atrace.sh
@@ -1,11 +1,18 @@
 # source code of atrace at http://androidxref.com/4.4.4_r1/xref/frameworks/native/cmds/atrace/atrace.cpp for Android 4.4.4 KitKat
 
-# enable sched, binder event tracing
-echo 1 > /sys/kernel/debug/tracing/events/sched/enable
-echo 1 > /sys/kernel/debug/tracing/events/binder/enable
+# start tracing for 10 sec with buffer size 100MB (2MB by default)
+echo 1 > /sys/kernel/debug/tracing/events/enable 
+atrace -t 25 -b 102400 > /sdcard/atrace.dump &
 
 # enable tracing all in atrace
 echo 1 > /sys/kernel/debug/tracing/events/enable 
 
-# start tracing for 10 sec with buffer size 100MB (2MB by default)
-atrace -t 10 -b 102400 > /sdcard/atrace.dump
+# dump trace
+cat /sys/kernel/debug/tracing/trace > /sdcard/atrace.dump
+
+# disable tracing all in atrace
+echo 0 > /sys/kernel/debug/tracing/events/enable 
+
+# enable sched, binder event tracing
+#echo 1 > /sys/kernel/debug/tracing/events/sched/enable
+#echo 1 > /sys/kernel/debug/tracing/events/binder/enable
