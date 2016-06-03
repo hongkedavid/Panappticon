797505840ac684437b0e816c6f7b2a58a780f4c0
David Ke Hong <hongkedavid@gmail.com>
2016-06-03 12:24:52 -0700

    Create android_dumpsys.sh

---
+++ script/android_dumpsys.sh
@@ -0,0 +1,8 @@
+# Dump memory info
+adb shell dumpsys meminfo
+
+# Dump memory info of an activity
+adb shell dumpsys meminfo com.google.android.gm
+
+# Dump UI info
+adb shell dumpsys activity com.google.android.gm
