0c5e0ddb5b661108b7a5d9faaff528394174fbd0
David Ke Hong <hongkedavid@gmail.com>
2016-06-03 15:11:46 -0700

    Update android_dumpsys.sh

--- script/android_dumpsys.sh
+++ script/android_dumpsys.sh
@@ -4,5 +4,8 @@ adb shell dumpsys meminfo
 # Dump memory info of an activity
 adb shell dumpsys meminfo com.google.android.gm
 
-# Dump UI info
+# Dump UI tree of an activity
 adb shell dumpsys activity com.google.android.gm
+
+# Dump UI rendering stat of an activity
+adb shell dumpsys gfxinfo com.google.android.gm
