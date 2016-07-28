3da31effdbf2b1b8f11758e6a2ba6f94e4fb3598
David Ke Hong <hongkedavid@gmail.com>
2016-07-27 21:32:02 -0700

    Update android_dmtracedump.sh

--- script/android_dmtracedump.sh
+++ script/android_dmtracedump.sh
@@ -1,5 +1,11 @@
 #adb shell setprop dalvik.vm.extra-opts -Xprofile:dualclock
+
+# Launch Android Device Monitor
 monitor
+
+# Alternatively, run traceview from command
+adb shell am profile com.example.app profile start /sdcard/trace_file
+adb shell am profile com.example.app profile stop
 
 dmtracedump -o $1.trace > $1.dump
 dmtracedump -o -g $1.png $1.trace > $1.profile
