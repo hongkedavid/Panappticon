bf55f33d121e6e2458bb31a28eef3b1f08e9dc91
David Ke Hong <hongkedavid@gmail.com>
2016-10-16 00:07:38 -0400

    Update android_dump_activity.sh

--- script/android_dump_activity.sh
+++ script/android_dump_activity.sh
@@ -1,7 +1,7 @@
 # Dump activity of an app given its APK
 # Ref: http://stackoverflow.com/questions/6547703/list-all-activities-within-an-apk-from-the-shell
 cd android-sdk-macosx/build-tools/23.0.0
-aapt dump xmltree <apk-file> AndroidManifest.xml
+./aapt dump xmltree <apk-file> AndroidManifest.xml
 
 # Get the main activity of an app given its APK
 # Ref: http://stackoverflow.com/questions/15497672/how-to-find-main-activity-in-apk-file-for-robotium-tests
