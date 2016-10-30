29fef784b5cf097b234e14c88a1cdbb0b328e2d2
David Ke Hong <hongkedavid@gmail.com>
2016-10-30 01:03:36 -0400

    Update ViewRootImpl.java

--- uitester/ViewRootImpl.java
+++ uitester/ViewRootImpl.java
@@ -1,4 +1,5 @@
 // Reference 1 https://developer.android.com/guide/topics/ui/how-android-draws.html
 // Reference 2 http://blog.csdn.net/feiduclear_up/article/details/46772477
 // Reference 3 http://blog.csdn.net/yanbober/article/details/46128379
+// Reference 4 http://blog.csdn.net/a553181867/article/details/51583060
 // UI update instrumentation on frameworks/base/core/java/android/view 
