64a7f5e3de947d1159d2a120854fa1c07f9039d0
David Ke Hong <hongkedavid@gmail.com>
2016-10-30 00:51:49 -0400

    Create ViewRootImpl.java

---
+++ uitester/ViewRootImpl.java
@@ -0,0 +1,2 @@
+# Reference from https://developer.android.com/guide/topics/ui/how-android-draws.html and http://blog.csdn.net/feiduclear_up/article/details/46772477
+# UI update instrumentation on frameworks/base/core/java/android/view 
