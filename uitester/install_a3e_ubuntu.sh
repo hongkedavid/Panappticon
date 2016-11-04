a7a8a5548dd1ae867ad50d578e068b4413fe8525
David Ke Hong <hongkedavid@gmail.com>
2016-11-04 17:11:46 -0400

    Create install_a3e_ubuntu.sh

---
+++ uitester/install_a3e_ubuntu.sh
@@ -0,0 +1,6 @@
+# Install Ruby 2.0 on Ubuntu 14.-4 by following http://stackoverflow.com/questions/26595620/how-to-install-ruby-2-1-4-on-ubuntu-14-04
+# Follow http://www.omgubuntu.co.uk/2016/03/fix-failed-to-fetch-google-chrome-apt-error-ubuntu and http://askubuntu.com/questions/766883/there-is-no-public-key-available-for-the-following-key-ids-1397bc53640db551 to fix erros with "apt-get update" on Ubuntu 14.04
+git clone https://github.com/tanzirul/a3e
+
+ruby bin/rec.rb ~/Downloads/umich_v3.3_apkpure.com.apk --no-rec -loop
+
