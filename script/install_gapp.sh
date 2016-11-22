982457541d1e05d2a398d3308ce5315f5ce33572
David Ke Hong <hongkedavid@gmail.com>
2016-11-22 02:26:31 -0500

    Update install_gapp.sh

--- script/install_gapp.sh
+++ script/install_gapp.sh
@@ -11,3 +11,7 @@ adb reboot recovery
 
 # Use the default setting (just swipping to confirm and go to next) and Choose "install zip" to install the Open Gapps zip file
 # After installation is done, click "Reboot system" to reboot
+
+# In AOSP build, you need to enable "com.google.android.gms" and "com.android.location.fused" by modifying some config files (frameworks/base/core/res/res/values/config.xml and device/moto/shamu/overlay/frameworks/base/core/res/res/values/config.xml) and rebuilding AOSP's framework
+# Ref 1: http://jhshi.me/2015/09/23/build-aosp-5-dot-1-1-for-nexus-5/index.html
+# Ref 2: http://forum.xda-developers.com/google-nexus-5/help/aosp-build-source-network-positioning-t2743517
