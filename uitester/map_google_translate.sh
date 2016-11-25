51d04a45a2497601e9643289112f6457c4168049
David Ke Hong <hongkedavid@gmail.com>
2016-11-25 06:17:02 -0500

    Update map_google_translate.sh

--- uitester/map_google_translate.sh
+++ uitester/map_google_translate.sh
@@ -1,10 +1,20 @@
 tid=3203
+func="SSL_read"
+for f in $(ls $tid.*traceview); 
+do 
+     echo $f; 
+     for tid in $(cat $f | grep "$func" | cut -d' ' -f1 | sort -n | uniq); 
+     do 
+         cat $f | grep "$tid " | head -n1; 
+     done; 
+done 
 
 # Extract relevant threads
-cat nexus4.translate.decoded | grep THREAD > thread_name.out
+file="nexus4.translate.decoded"
+cat $file | grep THREAD > thread_name.out
 ./sort_json.sh thread_name.out
 mv sorted.thread_name.out thread_name.out
-cat nexus4.translate.decoded | grep FORK | grep "tgid\":$tid}}" > fork.tid
+cat $file | grep FORK | grep "tgid\":$tid}}" > fork.tid
 ./sort_json.sh fork.tid
 mv sorted.fork.tid fork.tid
 
