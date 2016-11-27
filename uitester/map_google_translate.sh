48fb21f9be2363f86028288ff282211bcf822553
David Ke Hong <hongkedavid@gmail.com>
2016-11-26 19:05:01 -0500

    Update map_google_translate.sh

--- uitester/map_google_translate.sh
+++ uitester/map_google_translate.sh
@@ -17,6 +17,21 @@ mv sorted.thread_name.out thread_name.out
 cat $file | grep FORK | grep "tgid\":$tid}}" > fork.tid
 ./sort_json.sh fork.tid
 mv sorted.fork.tid fork.tid
+for f in $(ls $tid.*traceview); 
+do 
+    tname=$(grep S3LibThread $f | head -n2 | tail -n1 | cut -d' ' -f2); 
+    l=$(grep -n "$tname\"" thread_name.out | head -n1); c1=$(echo $l | cut -d':' -f1); 
+    t=$(echo $l | cut -d':' -f5 | cut -d',' -f1); 
+    for ((i=10;i<=15;i=i+1)); 
+    do 
+        if [ $(grep -n "$(($t+$i))" thread_name.out | wc -l) -gt 0 ]; then 
+            c2=$(grep -n "$(($t+$i))" thread_name.out | head -n1 | cut -d':' -f1); 
+            break; 
+        fi;
+    done
+    echo $f; 
+    cat thread_name.out | head -n$c2 | tail -n$(($c2-$c1)) | grep "pool\-" | grep "thread\-\"\|thread\""; 
+done
 
 # Extract relevant intervals
 
