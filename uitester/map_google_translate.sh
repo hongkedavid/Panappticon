28c8eef0fc73c6c21dc0744a159be2eb4b4b5e38
David Ke Hong <hongkedavid@gmail.com>
2016-11-27 22:53:10 -0500

    Update map_google_translate.sh

--- uitester/map_google_translate.sh
+++ uitester/map_google_translate.sh
@@ -50,6 +50,65 @@ do
 done
 rm javacron.tid
 
-# Extract relevant intervals
-
-# Compute resource features  
+# Extract relevant intervals and compute resource features  
+for f in $(ls ssl.thread.*);
+do
+    i=$(echo $f | cut -d'.' -f3)
+    rm $i.cpu_stat $i.sock_stat $i.disk_stat
+    for line in $(cat $f);
+    do
+        t=$(echo $line | cut -d',' -f1)
+        start=$(echo $line | cut -d',' -f2)
+        ptid=$(echo $line | cut -d',' -f3)
+        psec=$(echo $line | cut -d',' -f4)
+        pusec=$(echo $line | cut -d',' -f5)
+        trange=$(echo $psec | cut -c1-7);
+        echo $t, $start, $ptid, $psec, $pusec
+        cat nexus4.kernel.translate.decoded | grep "pid\":$ptid,\|new\":$ptid,\|pid\":$ptid}}" | grep "sec\":$trange" > tmp.trace
+        ./sort_json.sh tmp.trace
+        mv sorted.tmp.trace tmp.trace
+        cat $t.$i.out | grep SSL_read | grep "$t ent" | cut -c18-25 > tmp.1
+        cat $t.$i.out | grep SSL_read | grep "$t xit" | cut -c18-25 > tmp.2
+        paste -d',' tmp.1 tmp.2 > tmp.3
+        for l in $(cat tmp.3);
+        do
+            s=$(echo $l | cut -d',' -f1)
+            e=$(echo $l | cut -d',' -f2)
+            s1=$(($(($(($psec*1000000))+$pusec+$s-$start))/1000000))
+            s2=$(($(($(($psec*1000000))+$pusec+$s-$start))%1000000))
+            e1=$(($(($(($psec*1000000))+$pusec+$e-$start))/1000000))
+            e2=$(($(($(($psec*1000000))+$pusec+$e-$start))%1000000))
+            cat tmp.trace | python GrepTrace.py $s1 $s2 $e1 $e2 > sslthread.$i
+            ./profile_resource.sh sslthread.$i
+           cat sslthread.$i.cpu | python extractCPUResource.py $ptid >> $i.cpu_stat
+           cat sslthread.$i.sock | python extractIOResource.py $ptid >> $i.sock_stat
+           cat sslthread.$i.disk | python extractIOResource.py $ptid >> $i.disk_stat
+        done
+    done
+done
+for t in $(ls ssl.thread.* | cut -d'.' -f3 | sort -n);
+do
+   for ((j=2;j<=4;j=j+1));
+   do
+        c=0;
+        for f in $(cat $t.cpu_stat | cut -d' ' -f$j);
+        do
+            c=$(($c+$f));
+        done;
+        echo -n "$c " >> resource.csv;
+    done;
+    c=0;
+    for f in $(cat $t.sock_stat | cut -d' ' -f2);
+    do
+        c=$(($c+$f));
+    done;
+    echo -n "$c " >> resource.csv;
+    c=0
+    for f in $(cat $t.disk_stat | cut -d' ' -f2);
+    do
+        c=$(($c+$f));
+    done;
+    echo -n "$c" >> resource.csv;
+    echo "" >> resource.csv;
+done
+                                          
