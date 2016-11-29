9bfd7bd10ee031e3edb7a4ad177836f9839cda54
David Ke Hong <hongkedavid@gmail.com>
2016-11-29 02:19:11 -0500

    Create map_offerup.sh

---
+++ uitester/map_offerup.sh
@@ -0,0 +1,66 @@
+tid=8171
+
+# Extract relevant thread
+func="SSL_read"
+for f in $(ls $tid.*traceview); 
+do
+     echo $f
+     a=$(echo $f | cut -d'.' -f2)
+     for t in $(grep "$func" *.$a.out | cut -d':' -f1 | cut -d'.' -f1 | sort | uniq); 
+     do 
+         cat $f | grep "$t " | head -n1; 
+     done
+done > sslread.thread
+
+# Get input timestamp
+cat nexus4.user.offerup.decoded | grep UI_INPUT > nexus4.offerup.ui; ./sort_json.sh nexus4.offerup.ui
+for ((i=5;i<=$(cat nexus4.offerup.ui | wc -l);i=i+3)); do cat nexus4.offerup.ui | head -n$i | tail -n1 >> nexus4.offerup.ui.tmp; done
+mv nexus4.offerup.ui.tmp nexus4.offerup.ui
+
+file="nexus4.kernel.offerup.decoded"
+cat $file | grep THREAD > thread_name.out
+./sort_json.sh thread_name.out
+mv sorted.thread_name.out thread_name.out
+cat $file | grep FORK | grep ",\"tgid\":$tid}}" > fork.tid
+./sort_json.sh fork.tid
+mv sorted.fork.tid fork.tid
+
+rm pool12.thread
+for t in $(cat thread_name.out | grep "pool\-12\-thread" | cut -d'{' -f4 | cut -d':' -f2 | cut -d',' -f1); 
+do 
+    if [ $(cat fork.tid | grep "{\"pid\":$t," | wc -l) -gt 0 ]; then 
+       cat thread_name.out | grep "pool\-12\-thread" | grep "{\"pid\":$t," >> pool12.thread
+    fi
+done
+
+for i in $(cat trace.98 | grep CONTEXT | grep "\"I\"" | cut -d'{' -f3 | cut -d':' -f5 | cut -d',' -f1 | sort | uniq); do 
+cat thread_name.out | grep "{\"pid\":$i," | grep "Picasso\|pool\-12\-\|Retrofit\|Apptent\|IntentService\[R"; done
+
+for ((j=2;j<=102;j=j+1)); do echo $j; 
+for i in $(cat trace.$j | grep CONTEXT | grep "\"I\"" | cut -d'{' -f3 | cut -d':' -f5 | cut -d',' -f1 | sort | uniq); 
+do cat pool12.thread | grep "{\"pid\":$i," | grep "pool\-12\-"; done; done
+
+cat thread_name.out | grep "Apptentive-M" > apptent.thread
+for line in $(cat apptent.thread); do t=$(echo $line | cut -d'{' -f4 | cut -d':' -f2 | cut -d',' -f1); if [ $(cat fork.tid | grep "{\"pid\":$t," | wc -l) -gt 0 ]; then echo $line; fi; done > apptent.thread.tmp
+mv apptent.thread.tmp apptent.thread
+
+cat thread_name.out | grep "IntentService\[R" > intent.thread
+for line in $(cat intent.thread); do t=$(echo $line | cut -d'{' -f4 | cut -d':' -f2 | cut -d',' -f1); if [ $(cat fork.tid | grep "{\"pid\":$t," | wc -l) -gt 0 ]; then echo $line; fi; done > intent.thread.tmp
+mv intent.thread.tmp intent.thread
+
+k=1
+for line in $(cat nexus4.offerup.ui); 
+do 
+    a=$(echo $line | cut -d'{' -f3 | cut -d':' -f2 | cut -d',' -f1); 
+    b=$(echo $line | cut -d'{' -f3 | cut -d':' -f3 | cut -d'}' -f1); 
+    s=$(($(($a*1000000))+$b))
+    b=$(cat offerup.latency | head -n$k | tail -n1 | cut -d' ' -f2)
+    c=$(cat offerup.latency | head -n$k | tail -n1 | cut -d' ' -f1)
+    e=$(($(($(($s+$(($b*1000))))/1000000))+1))    
+    echo $a, $e
+    rm trace.$c
+    for ((i=$a;i<=$e;i=i+1)); do cat nexus4.offerup.decoded | grep "sec\":$i," >> trace.$c; done
+    ./sort_json.sh trace.$c
+    mv sorted.trace.$c trace.$c
+    k=$(($k+1))
+done
