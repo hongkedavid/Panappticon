60b025499b1ebbc435096ced5375cebd80d58543
David Ke Hong <hongkedavid@gmail.com>
2016-12-01 21:31:49 -0500

    Update map_flipp.sh

--- uitester/map_flipp.sh
+++ uitester/map_flipp.sh
@@ -189,3 +189,72 @@ do
     echo -n "$c" >> resource.csv;
     echo "" >> resource.csv;
 done
+
+
+
+func="Posix.connect"
+k=1
+for i in $(ls connect.thread.* | cut -d'.' -f3 | sort -n);
+do
+    f="connect.thread.$i"
+    rm $i.cpu_stat $i.sock_stat $i.disk_stat
+    for line in $(cat $f);
+    do
+        t=$(echo $line | cut -d',' -f1)
+        ptid=$(echo $line | cut -d',' -f2)
+        start=$(grep "ActivityThread.handleLaunchActivity" 1.$i.out | head -n1 | cut -c17-25 | sed 's/ //g')
+        psec=$(cat nexus4.flipp.ui | head -n$k | tail -n1 | cut -d'{' -f3 | cut -d':' -f2 | cut -d',' -f1)
+        pusec=$(cat nexus4.flipp.ui | head -n$k | tail -n1 | cut -d'{' -f3 | cut -d':' -f3 | cut -d'}' -f1)
+        echo $t, $start, $ptid, $psec, $pusec
+        cat trace.$i | grep "pid\":$ptid,\|new\":$ptid,\|pid\":$ptid}}" > tmp.trace
+        cat $t.$i.out | grep $func | grep "$t ent" | cut -c18-25 | sed 's/ //g' > tmp.1
+        cat $t.$i.out | grep $func | grep "$t xit" | cut -c18-25 | sed 's/ //g' > tmp.2
+        paste -d',' tmp.1 tmp.2 > tmp.3
+        for l in $(cat tmp.3);
+        do
+            s=$(echo $l | cut -d',' -f1)
+            e=$(echo $l | cut -d',' -f2)
+            s1=$(($(($(($psec*1000000))+$pusec+$s-$start))/1000000))
+            s2=$(($(($(($psec*1000000))+$pusec+$s-$start))%1000000))
+            e1=$(($(($(($psec*1000000))+$pusec+$e-$start))/1000000))
+            e2=$(($(($(($psec*1000000))+$pusec+$e-$start))%1000000))
+            echo $s1, $s2, $e1, $e2
+            cat tmp.trace | python GrepTrace.py $s1 $s2 $e1 $e2 > connectthread.$i
+            cat connectthread.$i | head -n1
+            cat connectthread.$i | tail -n1
+            ./profile_resource.sh connectthread.$i
+            cat connectthread.$i.cpu | python extractCPUResource.py $ptid >> $i.cpu_stat
+            cat connectthread.$i.sock | python extractIOResource.py $ptid >> $i.sock_stat
+            cat connectthread.$i.disk | python extractIOResource.py $ptid >> $i.disk_stat
+        done
+    done
+    rm connectthread.$i
+    k=$(($k+1))
+done
+rm tmp.trace tmp.1 tmp.2 tmp.3
+rm resource.csv
+for t in $(ls connect.thread.* | cut -d'.' -f3 | sort -n);
+do
+   for ((j=2;j<=4;j=j+1));
+   do
+        c=0;
+        for f in $(cat $t.cpu_stat | cut -d' ' -f$j);
+        do
+            c=$(($c+$f));
+        done;
+        echo -n "$c " >> resource.csv;
+    done;
+    c=0;
+    for f in $(cat $t.sock_stat | cut -d' ' -f2);
+    do
+        c=$(($c+$f));
+    done;
+    echo -n "$c " >> resource.csv;
+    c=0
+    for f in $(cat $t.disk_stat | cut -d' ' -f2);
+    do
+        c=$(($c+$f));
+    done;
+    echo -n "$c" >> resource.csv;
+    echo "" >> resource.csv;
+done
