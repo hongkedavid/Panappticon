18822530d3474d268394a70c7c542919793a5c02
David Ke Hong <hongkedavid@gmail.com>
2016-12-03 23:24:38 -0500

    Update map_offerup.sh

--- uitester/map_offerup.sh
+++ uitester/map_offerup.sh
@@ -26,10 +26,11 @@ cat $file | grep FORK | grep ",\"tgid\":$tid}}" > fork.tid
 ./sort_json.sh fork.tid
 mv sorted.fork.tid fork.tid
 
+#k=1
 for a in $(ls $tid.*traceview | cut -d'.' -f2 | sort -n); 
 do
     f=$(ls $tid.$a.*traceview)
-    isec=$(cat nexus4.offerup.ui | head -n$a | tail -n1 | cut -d'{' -f3 | cut -d':' -f2 | cut -d',' -f1)
+#    isec=$(cat nexus4.offerup.ui | head -n$k | tail -n1 | cut -d'{' -f3 | cut -d':' -f2 | cut -d',' -f1)
     for f in $(cat trace.$a | grep CONTEXT | grep "\"I\"" | cut -d':' -f7 | cut -d',' -f1 | sort -nr | uniq);  
     do      
         if [ $(cat fork.tid | grep "{\"pid\":$f," | wc -l) -gt 0 ]; then     
@@ -40,8 +41,9 @@ do
             echo "$ptid,$a,$tname,"
         fi  
     done
+#    k=$(($k+1))
 done > thread.map
-sed 's/ //g' thread.map
+sed -i 's/ //g' thread.map
 
 #########################
 rm pool12.thread
@@ -98,7 +100,7 @@ done
 #################################
 
 func="SSL_read"; file="ssl.thread"
-for a in $(ls $tid.*traceview | cut -d'.' -f2 | sort -n); 
+for a in $(ls $tid.100.*traceview | cut -d'.' -f2 | sort -n); 
 do
      f=$(ls $tid.$a.*traceview)
      rm $file.$a
@@ -112,12 +114,14 @@ do
          for l in $(cat thread.map | grep ",$a,"); 
          do
              l1=$(echo $l | cut -d',' -f2,3 | sed 's/\[/_/g')
-             if [ $tname = "Picasso-Idle" ] && [ $(echo $l1 | grep "Thread\-" | wc -l) -gt 0 ] && [ $(cat $f | grep "$l1" | wc -l) -eq 0 ]; then
+             if [ $tname = "Picasso-Idle" ] && [ $(echo $l1 | grep "Thread\-" | wc -l) -gt 0 ] && [ $(cat $f | grep "$(echo $l1 | cut -d',' -f2)" | wc -l) -eq 0 ]; then
+                 echo $l1
                  if [ $(cat $file.$a | grep ",$(echo $l | cut -d',' -f1)" | wc -l) -eq 0 ]; then 
                      ans=$(echo $l | cut -d',' -f1)
                      break
                  fi
-             elif [ $tname = "Retrofit-Idle" ] && [ $(echo $l1 | grep "pool\-" | wc -l) -gt 0 ] && [ $(cat $f | grep "$l1" | wc -l) -eq 0 ]; then
+             elif [ $tname = "Retrofit-Idle" ] && [ $(echo $l1 | grep "pool\-" | wc -l) -gt 0 ] && [ $(cat $f | grep "$(echo $l1 | cut -d',' -f2)" | wc -l) -eq 0 ]; then
+                 echo $l1
                  if [ $(cat $file.$a | grep ",$(echo $l | cut -d',' -f1)" | wc -l) -eq 0 ]; then 
                      ans=$(echo $l | cut -d',' -f1)
                      break
@@ -134,6 +138,7 @@ do
 done
 
 
+# Extract relevant intervals and compute resource features  
 k=1
 for line in $(cat nexus4.offerup.ui); 
 do 
@@ -150,3 +155,89 @@ do
     mv sorted.trace.$c trace.$c
     k=$(($k+1))
 done
+
+func="SSL_read"
+file="ssl.thread"
+k=1
+for i in $(ls $file.* | cut -d'.' -f3 | sort -n);
+do
+    f="$file.$i"
+    rm $i.cpu_stat $i.sock_stat $i.disk_stat
+    for line in $(cat $f);
+    do
+        t=$(echo $line | cut -d',' -f1)
+        ptid=$(echo $line | cut -d',' -f2)
+        start=$(grep "ActivityThread.handleLaunchActivity" 1.$i.out | head -n1 | cut -c17-25 | sed 's/ //g')
+        psec=$(cat nexus4.flipp.ui | head -n$k | tail -n1 | cut -d'{' -f3 | cut -d':' -f2 | cut -d',' -f1)
+        pusec=$(cat nexus4.flipp.ui | head -n$k | tail -n1 | cut -d'{' -f3 | cut -d':' -f3 | cut -d'}' -f1)
+#        echo $t, $start, $ptid, $psec, $pusec
+        cat trace.$i | grep "pid\":$ptid,\|new\":$ptid,\|pid\":$ptid}}" > tmp.trace
+        cat $t.$i.out | grep $func | grep "$t ent" | cut -c18-25 | sed 's/ //g' > tmp.1
+        cat $t.$i.out | grep $func | grep "$t xit" | cut -c18-25 | sed 's/ //g' > tmp.2
+        paste -d',' tmp.1 tmp.2 > tmp.3
+        for l in $(cat tmp.3);
+        do
+            s=$(echo $l | cut -d',' -f1)
+            e=$(echo $l | cut -d',' -f2)
+            s1=$(($(($(($psec*1000000))+$pusec+$s-$start))/1000000))
+            s2=$(($(($(($psec*1000000))+$pusec+$s-$start))%1000000))
+            e1=$(($(($(($psec*1000000))+$pusec+$e-$start))/1000000))
+            e2=$(($(($(($psec*1000000))+$pusec+$e-$start))%1000000))
+            echo $i, $t, $ptid, $s1, $s2, $e1, $e2
+            cat tmp.trace | python GrepTrace.py $s1 $s2 $e1 $e2 > sslthread.$i
+            if [ $(cat sslthread.$i | wc -l) -gt 0 ]; then
+               fline=$(cat sslthread.$i | head -n1)
+               ss1=$(echo $fline | cut -d'{' -f3 | cut -d':' -f2 | cut -d',' -f1)
+               ss2=$(echo $fline | cut -d'{' -f3 | cut -d':' -f3 | cut -d'}' -f1)
+               if [ $(($(($ss1*1000000))+$ss2-$(($s1*1000000))-$s2)) -gt 0 ]; then
+                  lc=$(grep -n "$fline" tmp.trace | cut -d':' -f1)
+                  cat tmp.trace | head -n$(($lc-1)) | tail -n1 > sslthread.$i.tmp
+                  cat sslthread.$i >> sslthread.$i.tmp
+                  mv sslthread.$i.tmp sslthread.$i
+               fi
+               fline=$(cat sslthread.$i | tail -n1)
+               ss1=$(echo $fline | cut -d'{' -f3 | cut -d':' -f2 | cut -d',' -f1)
+               ss2=$(echo $fline | cut -d'{' -f3 | cut -d':' -f3 | cut -d'}' -f1)
+                if [ $(($(($e1*1000000))+$e2-$(($ss1*1000000))-$ss2)) -gt 0 ]; then
+                  lc=$(grep -n "$fline" tmp.trace | cut -d':' -f1)
+                  cat tmp.trace | head -n$(($lc+1)) | tail -n1 >> sslthread.$i
+               fi
+            fi
+            cat sslthread.$i | head -n1
+            cat sslthread.$i | tail -n1
+            ./profile_resource.sh sslthread.$i
+            cat sslthread.$i.cpu | python extractCPUResource.py $ptid >> $i.cpu_stat
+            cat sslthread.$i.sock | python extractIOResource.py $ptid >> $i.sock_stat
+            cat sslthread.$i.disk | python extractIOResource.py $ptid >> $i.disk_stat
+        done
+    done
+    rm sslthread.$i*
+    k=$(($k+1))
+done
+rm tmp.trace tmp.1 tmp.2 tmp.3
+rm resource.csv
+for t in $(ls $file.* | cut -d'.' -f3 | sort -n);
+do
+   for ((j=2;j<=4;j=j+1));
+   do
+        c=0;
+        for f in $(cat $t.cpu_stat | cut -d' ' -f$j);
+        do
+            c=$(($c+$f));
+        done;
+        echo -n "$c " >> resource.csv;
+    done;
+    c=0;
+    for f in $(cat $t.sock_stat | cut -d' ' -f2);
+    do
+        c=$(($c+$f));
+    done;
+    echo -n "$c " >> resource.csv;
+    c=0
+    for f in $(cat $t.disk_stat | cut -d' ' -f2);
+    do
+        c=$(($c+$f));
+    done;
+    echo -n "$c" >> resource.csv;
+    echo "" >> resource.csv;
+done              
