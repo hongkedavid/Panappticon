d8fc1da3617e1efe7bed27d38bbe3f32dfb33ede
David Ke Hong <hongkedavid@gmail.com>
2017-02-15 23:25:48 -0500

    Update android_adb.sh

--- script/android_adb.sh
+++ script/android_adb.sh
@@ -1,2 +1,5 @@
 # Ref at http://adbshell.com/commands/adb-logcat
 adb logcat -v time -f /sdcard/logcat.out
+
+# Ref at http://stackoverflow.com/questions/16679944/do-c-objects-in-android-jni-native-code-invoke-garbage-collection
+adb logcat  -v threadtime -f /sdcard/logcat.out
