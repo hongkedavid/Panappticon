187a144f4ab0e24c2ed75f97f3968b64fa16e6cb
David Ke Hong <hongkedavid@gmail.com>
2017-02-21 22:58:06 -0500

    Update android_dumpsys.sh

--- script/android_dumpsys.sh
+++ script/android_dumpsys.sh
@@ -9,3 +9,15 @@ adb shell dumpsys activity com.google.android.gm
 
 # Dump UI rendering stat of an activity
 adb shell dumpsys gfxinfo com.google.android.gm
+
+# Dump running services of an app
+adb shell dumpsys activity services $app
+
+# Check if a service is running in an app
+adb shell dumpsys activity services $app.$service
+
+# Dump activity info of an app
+adb shell dumpsys activity $app
+
+# Dump an activity of an app
+adb shell dumpsys activity $app.$activity
