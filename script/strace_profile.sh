310d9e9c4cbd48732292b791542ac1a8e6431922
David Ke Hong <hongkedavid@gmail.com>
2017-02-28 15:11:32 -0500

    Update strace_profile.sh

--- script/strace_profile.sh
+++ script/strace_profile.sh
@@ -6,3 +6,6 @@ strace -p $pid -c -o /sdcard/$file
 
 # Log start time of each call
 strace -p $pid -ttt -o /sdcard/$file
+
+# Profile wall clock start time and run time of each call
+strace -p $pid -T -ttt -o /sdcard/$file
