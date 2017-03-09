d39416c48c1dd0a4ade385d261c87a9d89908bc7
David Ke Hong <hongkedavid@gmail.com>
2017-03-08 19:44:09 -0500

    Update strace_profile.sh

--- script/strace_profile.sh
+++ script/strace_profile.sh
@@ -9,3 +9,12 @@ strace -p $pid -ttt -o /sdcard/$file
 
 # Profile wall clock start time and run time of each call
 strace -p $pid -T -ttt -o /sdcard/$file
+
+
+fd=$(adb shell ls -l /proc/4727/fd | grep kgsl | cut -d':' -f2 | cut -d' ' -f2)
+c=0
+for s in $(cat ocr.strace.out | grep "ioctl($fd," | cut -d'=' -f2 | cut -d' ' -f3 | cut -d'<' -f2 | cut -d'>' -f1); 
+do 
+   c=$(echo "$c + $s" | bc)
+done
+echo $c
