e7714e335a49d67c0f33ab7a852928e707cf3410
David Ke Hong <hongkedavid@gmail.com>
2017-03-21 16:16:24 -0400

    Update strace_profile.sh

--- script/strace_profile.sh
+++ script/strace_profile.sh
@@ -1,17 +1,19 @@
-# Profile wall clock time of each call
+# Ref: https://linux.die.net/man/1/strace and https://strace.io/
+# Profile CPU time (spent running in the kernel) of each call
 strace -p $pid -T -o /sdcard/$file
 
 # Count total calls and CPU time
 strace -p $pid -c -o /sdcard/$file
 
-# Log start time of each call
+# Log (wall clock) start time of each call
 strace -p $pid -ttt -o /sdcard/$file
 
-# Profile wall clock start time and run time of each call
+# Profile (wall clock) start time and CPU time of each call
 strace -p $pid -T -ttt -o /sdcard/$file
 
-
-fd=$(adb shell ls -l /proc/4727/fd | grep kgsl | cut -d':' -f2 | cut -d' ' -f2)
+# Account total CPU time for ioctl 
+pid=4727
+fd=$(adb shell ls -l /proc/$pid/fd | grep kgsl | cut -d':' -f2 | cut -d' ' -f2)
 c=0
 for s in $(cat ocr.strace.out | grep "ioctl($fd," | cut -d'=' -f2 | cut -d' ' -f3 | cut -d'<' -f2 | cut -d'>' -f1); 
 do 
