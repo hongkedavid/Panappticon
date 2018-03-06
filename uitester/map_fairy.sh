42389352d82a8dfd0bd99cd0566b46e0f98fa646
David Ke Hong <hongkedavid@gmail.com>
2018-03-06 17:47:37 -0500

    Update map_fairy.sh

--- uitester/map_fairy.sh
+++ uitester/map_fairy.sh
@@ -19,3 +19,17 @@ done
 # Extract relevant threads
 for l in $(cat thread_name.out | grep "Thread\-"); do f=$(echo $l | cut -d':' -f7 | cut -d',' -f1); t=$(echo $l | cut -d':' -f4 | cut -d',' -f1); if [ $(cat fork.tid | grep "pid\":$f," | grep $t | wc -l) -gt 0 ]; then cat thread_name.out | grep "Thread\-" | grep "pid\":$f," | grep $t; cat fork.tid | grep "pid\":$f," | grep $t; fi; done > thread.tid
 i=1; for l in $(cat sorted.nexus4.fairy.ui | cut -d':' -f4 | cut -d',' -f1); do if [ $(cat thread.tid | grep FORK | grep $l | wc -l) -eq 1 ]; then ptid=$(cat thread.tid | grep FORK | grep $l | cut -d':' -f10 | cut -d',' -f1); cat trace.$i | grep "pid\":$ptid,\|new\":$ptid,\|pid\":$ptid}}" > trace.$i.$ptid; fi; i=$(($i+1)); done 
+
+# Profile resource usage
+for ll in $(cat fairy.latency);
+do
+    i=$(echo $ll | cut -d',' -f1)
+    t=$(echo $ll | cut -d',' -f2)
+    if [ $(ls trace.$i.* | wc -l) -eq 0 ]; then continue; fi
+    ptid=$(ls trace.$i.* | cut -d'.' -f3)
+    ./profile_resource.sh trace.$i.$ptid
+    rm $i.cpu_stat $i.sock_stat $i.disk_stat
+    cat trace.$i.$ptid.cpu | python extractCPUResource.py $ptid >> $i.cpu_stat
+    cat trace.$i.$ptid.sock | python extractIOResource.py $ptid >> $i.sock_stat
+    cat trace.$i.$ptid.disk | python extractIOResource.py $ptid >> $i.disk_stat
+done
