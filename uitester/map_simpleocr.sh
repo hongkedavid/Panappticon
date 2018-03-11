c69d3560bc573e9286895f2c2455702888400240
David Ke Hong <hongkedavid@gmail.com>
2018-03-10 19:42:23 -0500

    Update map_simpleocr.sh

--- uitester/map_simpleocr.sh
+++ uitester/map_simpleocr.sh
@@ -1,6 +1,21 @@
 for f in $(cat simpleocr.logcat.out | grep TouchEvent | cut -d':' -f4 | cut -d' ' -f22 | cut -c1-10); do cat sorted.nexus4.simpleocr.decoded | grep UI_INPUT | grep $f; done > sorted.nexus4.simpleocr.ui 
 for l in $(cat thread_name.out | grep "AsyncTask"); do f=$(echo $l | cut -d':' -f7 | cut -d',' -f1); t=$(echo $l | cut -d':' -f4 | cut -d',' -f1); if [ $(cat fork.tid | grep "pid\":$f," | grep $t | wc -l) -gt 0 ]; then cat thread_name.out | grep "AsyncTask" | grep "pid\":$f," | grep $t; cat fork.tid | grep "pid\":$f," | grep $t; fi; done > asynctask.tid
 
+# Segment trace
+for line in $(cat sorted.nexus4.simpleocr.ui); 
+do      
+    a=$(echo $line | cut -d'{' -f3 | cut -d':' -f2 | cut -d',' -f1);
+    b=$(echo $line | cut -d'{' -f3 | cut -d':' -f3 | cut -d'}' -f1);
+    s=$(($(($a*1000000))+$b));
+    b=$(cat simpleocr.latency | head -n$k | tail -n1 | cut -d',' -f2);
+    c=$(cat simpleocr.latency | head -n$k | tail -n1 | cut -d',' -f1);
+    e=$(($(($s+$(($b*1000))))/1000000));
+    echo $a, $e;
+    rm trace.$c;
+    for ((i=$a;i<=$e;i=i+1)); do cat sorted.nexus4.simpleocr.decoded | grep "sec\":$i," >> trace.$c; done;
+    k=$(($k+1));
+done
+
 # Extrace relevant thread
 cat asynctask.tid | grep THREAD | cut -d':' -f7 | cut -d',' -f1 > tmpid
 for i in $(cut -d',' -f1 simpleocr.latency); 
