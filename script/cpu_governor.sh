c91d59a8ee0130b58c88088edded8e76e99deec3
David Ke Hong <hongkedavid@gmail.com>
2018-03-12 03:04:58 -0400

    Update cpu_governor.sh

--- script/cpu_governor.sh
+++ script/cpu_governor.sh
@@ -9,6 +9,11 @@ echo "performance" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 echo "performance" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
 echo "performance" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
 
+echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+
 echo 1512000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo 1512000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 echo 1512000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
