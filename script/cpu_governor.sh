e6943712d6de8315d5d6f6b629902d548dafa2e6
David Ke Hong <hongkedavid@gmail.com>
2018-03-12 05:03:07 -0400

    Update cpu_governor.sh

--- script/cpu_governor.sh
+++ script/cpu_governor.sh
@@ -14,6 +14,7 @@ echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
 echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
 
+# Nexus 4 maximum CPU frequencey
 echo 1512000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo 1512000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 echo 1512000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
@@ -22,3 +23,9 @@ echo 1512000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor   
 cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq    
+
+# Nexus 6 maximum CPU frequencey
+echo 2649600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 2649600 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo 2649600 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo 2649600 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
